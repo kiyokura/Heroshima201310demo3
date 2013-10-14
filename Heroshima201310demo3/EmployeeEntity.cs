3587d720590298bb50697bdfbcf27b0cd4efdf31
kiyokura <kiyokura@gmail.com>
2013-10-14 22:36:34 +0900

    ソリューションファイルを登録

---
+++ Heroshima201310demo3/EmployeeEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Heroshima201310demo3
+{
+    public class EmployeeEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Heroshima201310demo3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.SqlServerCe;
+
+using Dapper;
+
+namespace Heroshima201310demo3
+{
+    class Program
+    {
+        static string constr = "Data Source=App_Data\\Database1.sdf";
+
+        static void Main(string[] args)
+        {
+
+            // パターン１
+            //using (var cn = new SqlCeConnection(constr))
+            //{
+            //    cn.Open();
+
+            //    var sql = "select ID, Name , Age , Email From Employee;";
+            //    var result = cn.Query(sql);
+
+            //    foreach (var d in result)
+            //    {
+            //        Console.WriteLine("ID:{0} , Name:{1} , Age:{2} , Email:{3}",
+            //                            d.ID,
+            //                            d.Name,
+            //                            d.Age,
+            //                            d.Email);
+            //    }
+            //}
+
+
+            // パターン2
+            //using (var cn = new SqlCeConnection(constr))
+            //{
+            //    cn.Open();
+            //    var sql = "select ID, Name , Age , Email From Employee;";
+            //    var result = cn.Query<EmployeeEntity>(sql);
+
+            //    foreach (var d in result)
+            //    {
+            //        Console.WriteLine("ID:{0} , Name:{1} , Age:{2} , Email:{3}",
+            //                            d.ID,
+            //                            d.Name,
+            //                            d.Age,
+            //                            d.Email);
+            //    }
+            //}
+
+            // パターン3
+            using (var cn = new SqlCeConnection(constr))
+            {
+                cn.Open();
+                var sql = "select ID, Name , Age , Email From Employee where Age > @Age;";
+                var result = cn.Query<EmployeeEntity>(sql, new { Age = 25 });
+
+                foreach (var d in result)
+                {
+                    Console.WriteLine("ID:{0} , Name:{1} , Age:{2} , Email:{3}",
+                                        d.ID,
+                                        d.Name,
+                                        d.Age,
+                                        d.Email);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+}
